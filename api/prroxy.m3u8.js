9f31e4006dae4b4e3a468390286db2b8c45e89c9
StevoSure123 <stevosure123@gmail.com>
2024-12-11 16:18:43 +0600

    Update prroxy.m3u8.js

--- api/prroxy.m3u8.js
+++ api/prroxy.m3u8.js
@@ -1,20 +1,25 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-    // Retrieve the URL of the proxied M3U8 file from the query parameter
-    const proxiedM3U8Url = req.query.url;
-
-    if (!proxiedM3U8Url) {
-        return res.status(400).send('Missing URL parameter');
-    }
-
     try {
+        // Retrieve the URL of the proxied M3U8 file from the query parameter
+        const proxiedM3U8Url = req.query.url;
+        if (!proxiedM3U8Url) {
+            console.error('Missing URL parameter');
+            return res.status(400).send('Missing URL parameter');
+        }
+
+        console.log('Fetching M3U8 file from:', proxiedM3U8Url);
+
         // Fetch the original proxied M3U8 file
         const response = await axios.get(proxiedM3U8Url, {
             headers: {
                 'Referer': req.query.headers ? JSON.parse(req.query.headers).referer : ''
             }
         });
+
+        // Log the response data
+        console.log('M3U8 file fetched successfully, replacing "ts" with "m3u8"');
 
         // Replace all occurrences of 'ts' with 'm3u8' in the playlist content
         const modifiedPlaylist = response.data.replace(/\bts\b/g, 'm3u8');
@@ -24,9 +29,8 @@ module.exports = async (req, res) => {
 
         // Send the modified M3U8 file
         res.send(modifiedPlaylist);
-
     } catch (error) {
-        console.error('Error fetching M3U8 playlist:', error);
+        console.error('Error fetching or modifying the M3U8 file:', error.message);
         res.status(500).send('Error fetching or modifying the playlist');
     }
 };
